94b5d22928a09104613cba754ad49fb45a9b04fd
Ankit <ankit.7002604485@gmail.com>
2026-02-23 13:14:19 +0530

    Cors implemented

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors')
 
 const cookieParser = require("cookie-parser")
 
@@ -6,6 +7,7 @@ const cookieParser = require("cookie-parser")
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors())
 
 // Routes Imported
 const authRouter = require("./routes/auth.routes");
