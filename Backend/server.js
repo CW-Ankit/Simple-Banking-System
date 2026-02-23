826410fbe1d75361fc579587cf0c1388418902e0
Ankit <ankit.7002604485@gmail.com>
2026-02-23 16:46:17 +0530

    Port Configered

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectToDB = require("./src/config/db")
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 connectToDB()
 
